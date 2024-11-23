40b05b534d6007e117d7fc420713cf58d0128052
Fernando GJ <fernandogajib@gmail.com>
2024-11-23 21:00:02 +0100

    Storing all the constant variables of the game - Components size (paddle, jpanel, ball) - Game text . .

---
+++ src/controller/GameConst.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package controller;
+
+import java.awt.Dimension;
+
+/**
+ *
+ * @author Fernando GJ
+ */
+public class GameConst {
+    public final static int FPS = 60;
+    public final static int INITIAL_BALL_SPEED = 4;
+    public final static int PADDLE_SPEED = 5;
+    
+    public static class Size {
+
+        private static final int BOARD_WIDHT = 1000;
+        private static final int BOARD_HEIGHT = (int) (BOARD_WIDHT * 0.55);
+
+        public static final Dimension BOARD_SIZE = new Dimension(BOARD_WIDHT, BOARD_HEIGHT);
+        public static final int BALL_DIAMETER = 20;
+        public static final Dimension PADDLE_SIZE = new Dimension(15, 100);
+        
+    }
+    
+    public static class Movement {
+        
+        public final static int NO_MOVEMENT = -1;
+        public final static int UP = 0;
+        public final static int DOWN = 1;
+        
+    }
+    
+    public static class Text {
+        
+        public final static String BEFORE_STARTING = "Press SPACE to start the game";
+    }
+
+} // end GameConst
