fd0241628b705ca7d39b06dab18e156464677d69
Fernando GJ <fernandogajib@gmail.com>
2024-11-29 19:27:33 +0100

    Interface to say the actions will do the game logic classes (ball and paddle)

---
+++ src/model/ItselfAction.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+package model;
+
+import java.awt.Graphics;
+
+/**
+ *
+ * @author Fernando GJ
+ */
+public interface ItselfAction {
+    
+    void update();
+    void render(Graphics g);
+
+} // end ItselfAction
